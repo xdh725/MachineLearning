84de363ec9dc6aa0803f6b710b10afaac6f103a7
JayXie <xdh725@126.com>
2017-11-16 19:23:26 +0800

    添加类与方法体的注释

--- P2/entertainment_center.py
+++ P2/entertainment_center.py
@@ -1,23 +1,29 @@
+# -*- coding: UTF-8 -*-
 
 from media import Movie
 from fresh_tomatoes import *
 
+# 方法体 创建并返回包含两个电影对象的对象数组 
 def createMovies():
 
+    #初始化电影对象实例1
     movie1 = Movie()
     movie1.trailer_url = "https://v.youku.com/v_show/id_XMzE1NzYzMTg1Mg==.html"
-    movie1.title = "HeadHunter"
+    movie1.title = "HeadHunting"
     movie1.poster_image_url = "https://vthumb.ykimg.com/vi/XMzE1NzYzMTg1Mg==/89/default.jpg"
 
+    #初始化电影对象实例2
     movie2 = Movie()
     movie2.trailer_url = "https://v.youku.com/v_show/id_XMzE1OTA2NjI4OA==.html"
     movie2.title = "chenqiaoen"
     movie2.poster_image_url = "https://vthumb.ykimg.com/vi/XMzE1OTA2NjI4OA==/89/default.jpg"
 
+    #初始化一个电影对象数组
     movies = [movie1, movie2]
 
     return movies
 
+#创建包含电影信息的数组
 movieInfo = createMovies()
 
 # for movie in movieInfo:
@@ -27,4 +33,5 @@ movieInfo = createMovies()
 
 # print content
 
+#将包含电影信息的数组数据传入open_movies_page方法中 先生成一个html文件并打开
 open_movies_page(movieInfo)
--- P2/media.py
+++ P2/media.py
@@ -1,13 +1,17 @@
+# -*- coding: UTF-8 -*-
 
+# 电影信息对象类
 class Movie(object):
     """docstring for Movie."""
 
+    #类初始化 并对三个成员变量赋值 预设默认值
     def __init__(self, title='UnkownMovie', poster_image_url='', trailer_url=''):
         super(Movie, self).__init__()
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_url = trailer_url
 
+    #测试用 打印类的成员变量信息
     def description(self):
         print('title: ' + self.title + '\nposter_image_url: ' + self.poster_image_url + '\ntrailer_url:' + self.trailer_url)
         pass
